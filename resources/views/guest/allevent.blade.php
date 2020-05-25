a318a05e00f12746554073d606943b5a17bb59d9
unknown <sathasivam@hridhamtech.com>
2020-05-25 18:56:52 +0530

    all

--- resources/views/guest/allevent.blade.php
+++ resources/views/guest/allevent.blade.php
@@ -41,7 +41,8 @@
                             <br><br>
                             <div class="col-md-10  col-xs-offset-1" style="background-color:#FFBF00;min-height: 200px">
                                 <p style="color: white"><b>{{ $event->event_title }}</b></p>
-                                <p style="color: white">{{ str_limit(strip_tags($event->event_desc), 120) }}</p>
+                                <p style="color: white">{{ $event->event_desc }}</p>
+                            
                                 {{-- <a href="{{ route('guest.show',['event'=>$event->id]) }}"> <button type="button"
                                     class="btn btn-white">Details</button></a> --}}
                             </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,10 +56,12 @@
                                     <div class="col-md-10  col-xs-offset-1"
                                         style="background-color:#FFBF00;padding-top:20px;padding-bottom:20px ;min-height: 200px">
                                         <p style="color: white"><b>{{ $event->event_title }}</b></p>
-                                        <p style="color: white">{{ str_limit(strip_tags($event->event_desc), 120) }}</p>
+                                        <p style="color: white">{{ $event->event_desc }}</p>
+                                      
                                         {{-- <a
                                             href="{{ route('guest.show',['event'=>$event->id]) }}">
                                             <button type="button" class="btn btn-white">Details</button></a> --}}
+                                            
                                     </div>
                                 </div>
                             @endforeach
