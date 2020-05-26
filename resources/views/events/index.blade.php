f992d8c4eff6464df8afee80ef716ee7b914909d
k2gsalem <k2gsalem@gmail.com>
2020-05-26 18:40:06 +0530

    sno

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,10 +1,10 @@
 @extends('master')
 @section('bootstrap')
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
 
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/fontAwesome.css">
-        <link rel="stylesheet" href="css/templatemo-style.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/fontAwesome.css">
+<link rel="stylesheet" href="css/templatemo-style.css">
 
 
 
@@ -13,17 +13,19 @@
 
 @section('page-content')
 <style>
-.overlay{
-    background-color: white;
-}
-    </style>
+    .overlay {
+        background-color: white;
+    }
+
+</style>
 
 <div class="container">
 
 
     <table id="example" class="table table-striped table-bordered" style="width:100%">
         <div style="margin-bottom: 20px">
-            <a href="{{ route('events.create') }}"><button class="btn btn-primary"> Add New</button></a>
+            <a href="{{ route('events.create') }}"><button class="btn btn-primary"> Add
+                    New</button></a>
         </div>
         <thead>
             <tr>
@@ -34,37 +36,50 @@
                 <th>Event Presenter Designation</th>
                 <th>Organization Details</th>
                 <th>Event Date</th>
-         <th>Show </th>
+                <th>Show </th>
                 <th>Edit</th>
                 <th>Delete</th>
             </tr>
         </thead>
         <tbody>
+            @php
+                $sno=1;
+            @endphp
             @foreach($events as $event)
-                <tr>
-                    <td>{{  $event->id }}</td>
-                    <td>{{  $event->event_title }}</td>
-                    <td>{{  $event->event_desc }}</td>
 
-                    <td>{{  $event->event_presenter_name }}</td>
-                    <td>{{  $event->event_presenter_designation }}</td>
-                    <td>{{  $event->event_presenter_org_details }}</td>
-                    <td>{{  $event->event_date }}</td>
-                   <!-- <td><a href="{{ route('events.create') }}"> ADD</a></td>-->
-                 <td><a href="{{ route('events.show',['event'=>$event->id])  }}"> Show</a></td>
-                    <td><a href="{{ route('events.edit',['event'=>$event->id])  }}"> Edit</a></td>
+                <tr>
+                    <td>{{ $sno }}</td>
+                    <td>{{ $event->event_title }}</td>
+                    <td>{{ $event->event_desc }}</td>
+
+                    <td>{{ $event->event_presenter_name }}</td>
+                    <td>{{ $event->event_presenter_designation }}</td>
+                    <td>{{ $event->event_presenter_org_details }}</td>
+                    <td>{{ $event->event_date }}</td>
+                    <!-- <td><a href="{{ route('events.create') }}"> ADD</a></td>-->
+                    <td><a
+                            href="{{ route('events.show',['event'=>$event->id]) }}">
+                            Show</a></td>
+                    <td><a
+                            href="{{ route('events.edit',['event'=>$event->id]) }}">
+                            Edit</a></td>
                     <td>
-                    <form action="{{ route('events.destroy',['event'=>$event->id]) }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" value="Delete">
-                    </form>
+                        <form
+                            action="{{ route('events.destroy',['event'=>$event->id]) }}"
+                            method="post">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="Delete">
+                        </form>
                     </td>
                 </tr>
+                @php
+                    $sno++;
+                @endphp
             @endforeach
         </tbody>
     </table>
-</body>
+    </body>
 
-</html>
-@endsection
+    </html>
+    @endsection
